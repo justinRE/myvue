67654b4eaf8a1362b299a766a6a6cf060bb7f0f4
JustinRE <justin@wesleyreisz.com>
2023-04-21 12:50:43 -0400

    Secret key addd to additional page

--- src/views/AdditionalView.vue
+++ src/views/AdditionalView.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="additional">
         <h1>This is the updated information</h1>
+        <h2> This is a secret key: {{ Key }}</h2>
     </div>
 </template>
+
+<script>
+export default{
+    name:'AdditionalView',
+computed:{
+    Key(){
+        return process.env.VUE_APP_KEY
+    }
+}
+}
+</script>
